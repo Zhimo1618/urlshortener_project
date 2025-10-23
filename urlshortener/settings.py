34adcce0751689a3959ab5ab8400561b2870e78a
Zhimo1618 <aston1.618@gmail.com>
2025-10-24 01:33:38 +0800

    修改env要求的參數與地端資料庫路徑

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'urlshortener.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / env('SQLITE_PATH', 'db.sqlite3'),
+        'NAME': BASE_DIR / os.getenv('SQLITE_PATH', 'db.sqlite3'),
     }
 }
 
